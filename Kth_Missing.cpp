90a9d453a32dd45a0fa0550c448fdf5588620689
Aryan <aryan.nigania@gmail.com>
2026-01-15 12:27:55 +0530

    Add files via upload

---
+++ Kth_Missing.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingK(vector<int>& nums, int k) {
+    int n = nums.size();
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // numbers missing till index mid
+        int missing = nums[mid] - (mid + 1);
+
+        if (missing < k) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return k + high + 1;
+}
+
+int main() {
+    vector<int> vec = {2, 3, 4, 7, 11};
+    int k = 5;
+
+    cout << missingK(vec, k) << endl;
+    return 0;
+}
